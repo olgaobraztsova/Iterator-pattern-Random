75caedd89f92bba05112d58f35b087bc79f921ac
olgaobraztsova <olga.obraztsova@gmail.com>
2022-06-22 23:00:10 +0530

    Initial Commit

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Randoms {
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    private final int min;
+    private final int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        random = new Random();
+    }
+
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min) + min + 1;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+
 }
